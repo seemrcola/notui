e01ee3fe1e34671137862979b8b6078217876a38
seemrcola <seemrcola@163.com>
2023-07-03 18:15:03 +0800

    feat: moveable comp

---
+++ packages/components/Moveable/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from '@notui/utils'
+import Moveable from './src/index.vue'
+
+export const NoMoveable = withInstall(Moveable)
+export default NoMoveable
+
+export * from './src/props'
--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang='ts'>
-import { Repl as REPL } from '@vue/repl'
-
-// @ts-expect-error
-import CodeMirror from '@vue/repl/codemirror-editor'
-import '@vue/repl/style.css'
+import NoMoveable from '@notui/components/Moveable'
+import Tiresome from '@notui/components/TiresomeTag'
+import Icon from '@notui/components/Icon'
 import Header from './components/Header.vue'
 </script>
 
 <template>
   <Header bg="gray-100" />
   <div w-full h="[calc(100vh-60px)]" flex-center>
-    <REPL w-full :editor="CodeMirror" />
+    <Icon name="i-material-symbols:bookmark-remove-outline" />
+    <NoMoveable text-center>
+      1000
+    </NoMoveable>
+    <Tiresome />
   </div>
 </template>
